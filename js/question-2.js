a12025eb4a06a91158f328311c2aeb76a9d6dbec
pretzL <ericpretzinger@gmail.com>
2022-02-09 23:49:52 +0100

    initial setup

---
+++ js/question-2.js
@@ -0,0 +1,12 @@
+/* Question 2
+Make a call to the Rawg API.
+Go to https://rawg.io/apidocs and get an API key which you’ll use as part of the endpoint you’re making an API call to. 
+You can use https://noroff.no for the URL and Noroff Assignment for the description.
+You'll be given an API Key you can add as a "key" parameter in your fetch request.
+Make a call to the following API endpoint replacing INSERTAPIKEYHERE with the key given to you from the Rawg API.
+https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=INSERTAPIKEYHERE
+
+Loop through the results and display the following properties in HTML, but only for the first eight results:
+name
+rating
+number of tags (not the tag details, just the amount of tags) */
